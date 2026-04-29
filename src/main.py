d17d602e9cc12edfca687c45c5f073fd25583c76
igorCol <igor.colombini@gmail.com>
2026-04-28 22:27:18 -0300

    MAIN.PY orchestrator

--- src/main.py
+++ src/main.py
@@ -2,45 +2,59 @@ import os
 import logging
 from extractor import IDMLExtractor
 from translator import GeminiTranslator
+from builder import IDMLBuilder
 
-logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
 
 def main():
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     idml_file = os.path.join(root_dir, "data", "manual.idml")
+    output_file = os.path.join(root_dir, "data", "manual_pt.idml")
 
-    if not os.path.exists(idml_file):
-        logging.error(f"Arquivo não encontrado: {idml_file}")
+    if not  os.path.exists(idml_file):
+        logging.error(f"Arquivo não encontraro: {idml_file}")
         return
-
+    
     extractor = IDMLExtractor(idml_file)
-    translator = GeminiTranslator(batch_size=5)  # Lote de 5 apenas para o teste
+    translator = GeminiTranslator(batch_size=50)
 
     try:
+        # Extração
         extractor.unzip()
         stories = extractor.get_story_files()
 
-        if stories:
-            # 1. Extração
-            payload = extractor.build_memory_map(stories)
+        if not stories:
+            logging.error("Nenhuam story encontrada.")
+            return
+        
+        payload = extractor.build_memory_map(stories)
 
-            # Pegando apenas 5 itens de teste
-            test_payload = payload[:5]
-            text_only_batch = [item["original_text"] for item in test_payload]
+        # Preparação de Lotes
+        text_only_list = [item["original_text"] for item in payload]
+        batches = translator.create_batches(text_only_list)
+        all_translations = []
 
-            logging.info(f"Enviando lote de teste para a API...")
-            logging.info(f"Original: {text_only_batch}")
+        # Processamento na IA
+        logging.info(f"Iniciação tradução em massa de {len(batches)} lotes...")
+        for i, batch in enumerate(batches):
+            logging.info(f"Processando lote {i+1}/{len(batches)}...")
+            translated_batch = translator.translate_batch(batch)
+            all_translations.extend(translated_batch)
 
-            # 2. Tradução via IA
-            translated_batch = translator.translate_batch(text_only_batch)
+        # Reconstrução do IDML
+        builder = IDMLBuilder(extractor.temp_dir, extractor.xml_trees)
+        builder.inject_translation(payload, all_translations)
+        builder.save_xml_files()
+        builder.repackage()
 
-            logging.info(f"Tradução concluída!")
-            logging.info(f"Retorno: {translated_batch}")
+        logging.info(f"SUCESSO!! O manual traduzido está em: {output_file}")
 
+    except Exception as e:
+        logging.error(f"O pipeline falhou: {e}")
+        
     finally:
         extractor.cleanup()
-
 
 if __name__ == "__main__":
     main()
